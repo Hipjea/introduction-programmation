6f1d8afa44f7602c932458d82f8b489a5ee11d93
Hipjea <pierre.duverneix@gmail.com>
2023-04-29 15:45:09 +0200

    syntax

--- print/1-affichage_simple.py
+++ print/1-affichage_simple.py
@@ -8,5 +8,6 @@ print(salutation)
 nom = "utilisateur"
 # Enchaînement de paramètres dans print()
 print(salutation, ",", nom)
+
 # En utilisant l'opérateur + pour éviter les ajouts automatiques d'espaces entre les variables
 print(salutation + ", " + nom)
